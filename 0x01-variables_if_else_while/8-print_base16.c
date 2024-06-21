ea9a7be1c8cb9d7998085713055b0eaebd47b638
R-INYURU <rwema.fred@tutamail.com>
2024-06-21 01:17:22 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - starting block
+ *
+ * Return: void(0)
+ */
+int main(void)
+{
+	int n = 48;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+
+	for (n = 97; n < 103; n++)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
