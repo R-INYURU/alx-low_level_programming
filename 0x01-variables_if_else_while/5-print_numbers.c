d4816abb60342aea1ed18a9ccf8ff7e1f75cc856
R-INYURU <rwema.fred@tutamail.com>
2024-06-21 01:07:42 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - starting block
+ *
+ * Return: void(0)
+ */
+int main(void)
+{
+	int n = 48;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
