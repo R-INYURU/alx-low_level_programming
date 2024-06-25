c713a7cf61298ada4788fd1e6fb16fc75d7c14f9
R-INYURU <rwema.fred@tutamail.com>
2024-06-25 10:42:30 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers and return result
+ * @n1: first interger
+ * @n2: second interger
+ *
+ * Return: sum
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
