c218406034f4eb105e2265509dbf15262d32d52a
R-INYURU <rwema.fred@tutamail.com>
2024-06-26 01:10:10 -0700

    even when they are equal

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,12 +12,36 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
-		largest = a;
-	else if (b > a && b > c)
-		largest = b;
-	else if (c > b && c > a)
-		largest = c;
+	if (a == b)
+	{
+		if (a > c)
+			largest = a;
+		else
+			largest = c;
+	}
+	else if (a == c)
+	{
+		if (a > b)
+			largest = a;
+		else
+			largest = b;
+	}
+	else if (b == c)
+	{
+		if (a > b)
+			largest = a;
+		else
+			largest = b;
+	}
+	else
+	{
+		if (a > b && a > c)
+			largest = a;
+		else if (b > a && b > c)
+			largest = b;
+		else
+			largest = c;
+	}
 
 	return (largest);
 }
