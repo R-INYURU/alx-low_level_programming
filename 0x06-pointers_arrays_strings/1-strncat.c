f17e3c4b4b74b51ac567bdd5d5c15e9f77069a66
R-INYURU <rwema.fred@tutamail.com>
2024-07-03 13:47:26 -0700

    no smaching stack

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,19 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 	int count = 0;
+	int cnt = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		count++;
+	for (i = 0; src[i] != '\0'; i++)
+		cnt++;
 
 	for (i = count, j = 0; j < n; i++, j++)
+	{
+		if (j > cnt)
+			break;
 		dest[i] = src[j];
+	}
 	dest[count + n] = '\0';
 
 	return (dest);
