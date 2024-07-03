84284a276219a80f48552869ff551c64492b52d4
R-INYURU <rwema.fred@tutamail.com>
2024-07-02 18:20:32 -0700

    in the millls

--- 0x05-pointers_arrays_strings/8-main.c
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -7,13 +7,15 @@
  */
 int main(void)
 {
-	int array[5];
+	int array[7];
 
 	array[0] = 98;
 	array[1] = 402;
 	array[2] = -198;
 	array[3] = 298;
 	array[4] = -1024;
-	print_array(array, 5);
+	array[5] = 2984535;
+	array[6] = 3554257;
+	print_array(array, 7);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,82 @@
 #include "main.h"
 
 /**
+ * print_m - print numb of millions
+ * @numb: number to print
+ *
+ * Return: void
+ */
+void print_m(int numb)
+{
+	int rmndr, tns, hndrd, thsnd, tnthsnd, hndthsnd, mills;
+
+	if (numb > 999999 && numb < 10000000)
+	{
+		mills = numb / 1000000;
+		hndthsnd = (numb - (mills * 1000000)) / 100000;
+		tnthsnd = (numb - ((mills * 1000000) + (hndthsnd * 100000))) / 10000;
+		thsnd = (numb - ((mills * 1000000) + (hndthsnd * 100000) +
+					 (tnthsnd * 10000))) / 1000;
+		hndrd = (numb - ((mills * 1000000) + (hndthsnd * 100000) +
+					 (tnthsnd * 10000) + (thsnd * 1000))) / 100;
+		tns = (numb - ((mills * 1000000) + (hndthsnd * 100000) +
+				   (tnthsnd * 10000) + (thsnd * 1000) + (hndrd * 100))) / 10;
+		rmndr = numb % 10;
+		_putchar(mills + 48);
+		_putchar(hndthsnd + 48);
+		_putchar(tnthsnd + 48);
+		_putchar(thsnd + 48);
+		_putchar(hndrd + 48);
+		_putchar(tns + 48);
+		_putchar(rmndr + 48);
+	}
+}
+
+/**
+ * print_mill - print numb into mills with _putchar
+ * @numb: numb to print
+ *
+ * Return: void
+ */
+void print_mill(int numb)
+{
+	int rmndr, tns, hndrd, thsnd, tnthsnd, hndthsnd;
+
+	if (numb > 9999 && numb < 10000)
+	{
+		tnthsnd = numb / 10000;
+		thsnd = (numb - (tnthsnd * 10000)) / 1000;
+		hndrd = (numb - ((tnthsnd * 10000) + (thsnd * 1000))) / 100;
+		tns = (numb - ((tnthsnd * 10000) + (thsnd * 1000) +
+					   (hndrd * 100))) / 10;
+		rmndr = numb % 10;
+		_putchar(tnthsnd + 48);
+		_putchar(thsnd + 48);
+		_putchar(tns + 48);
+		_putchar(rmndr + 48);
+	}
+	else if (numb > 99999 && numb < 1000000)
+	{
+		hndthsnd = numb / 100000;
+		tnthsnd = (numb - (hndthsnd * 100000)) / 10000;
+		thsnd = (numb - ((hndthsnd * 100000) + (tnthsnd * 10000))) / 1000;
+		hndrd = (numb - ((hndthsnd * 100000) + (tnthsnd * 10000) +
+						 (thsnd * 1000))) / 100;
+		tns = (numb - ((hndthsnd * 100000) + (tnthsnd * 10000) +
+					   (thsnd * 1000) + (hndrd * 100))) / 10;
+		rmndr = numb % 10;
+		_putchar(hndthsnd + 48);
+		_putchar(tnthsnd + 48);
+		_putchar(thsnd + 48);
+		_putchar(hndrd + 48);
+		_putchar(tns + 48);
+		_putchar(rmndr + 48);
+	}
+	else if (numb > 999999 && numb < 10000000)
+		print_m(numb);
+}
+
+/**
  * prints - print numb with _putchar
  * @numb: number to print
  *
@@ -45,6 +121,8 @@ void prints(int numb)
 		_putchar(tns + 48);
 		_putchar(rmndr + 48);
 	}
+	else
+		print_mill(numb);
 }
 
 /**
@@ -58,18 +136,23 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n < 0)
+		_putchar('\n');
+	else
 	{
-		if (i != (n  - 1))
+		for (i = 0; i < n; i++)
 		{
-			prints(a[i]);
-			_putchar(',');
-			_putchar(' ');
-		}
-		else
-		{
-			prints(a[i]);
-			_putchar('\n');
+			if (i != (n  - 1))
+			{
+				prints(a[i]);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				prints(a[i]);
+				_putchar('\n');
+			}
 		}
 	}
 }
