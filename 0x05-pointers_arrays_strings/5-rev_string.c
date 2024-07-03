ce91c2080028a125c0eab440b92a41a87f32fc52
R-INYURU <rwema.fred@tutamail.com>
2024-07-02 19:26:05 -0700

    just refer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,10 +15,22 @@ void rev_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 		count++;
 
-	for (i = (count - 1); i > (count / 2); i--)
+	if (count % 2 != 0)
 	{
-		c = s[i];
-		s[i] = s[(count - 1) - i];
-		s[(count - 1) - i] = c;
+		for (i = (count - 1); i > (count / 2); i--)
+		{
+			c = s[i];
+			s[i] = s[(count - 1) - i];
+			s[(count - 1) - i] = c;
+		}
+	}
+	else
+	{
+		for (i = (count - 1); i >= (count / 2); i--)
+		{
+			c = s[i];
+			s[i] = s[(count - 1) - i];
+			s[(count - 1) - i] = c;
+		}
 	}
 }
