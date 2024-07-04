e6e690c809e44b83330d32a5f1e13adb2f6b9d6a
R-INYURU <rwema.fred@tutamail.com>
2024-07-03 21:43:28 -0700

    always look up

---
+++ 0x06-pointers_arrays_strings/5-string-toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase letters to uppercase
+ * @str: string to convert to uppercase
+ *
+ * Return: pointer to the converted string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+	int count = 0;
+	char *newstr = str;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count ++;
+
+	for (i = 0; i < count; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+			newstr[i] = str[i] - 32;
+		else
+			newstr[i] = str[i];
+	}
+	newstr[count + 1] = '\0';
+	return (newstr);
+}
