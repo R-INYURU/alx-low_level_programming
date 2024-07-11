d0cba26008874759c089db3f875dae791b2de042
R-INYURU <rwema.fred@tutamail.com>
2024-07-11 07:52:19 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to string
+ * @c: character to locate
+ *
+ * Return: pointer to first occurence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != c; i++);
+
+	return (&s[i]);
+}
