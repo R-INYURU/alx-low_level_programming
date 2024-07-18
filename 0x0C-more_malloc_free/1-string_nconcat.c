23c38b7117b5d77bc813fa11b3530e31cbb7c8a8
R-INYURU <rwema.fred@tutamail.com>
2024-07-18 14:57:56 -0700

    bit*h betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!newstr)
 		return (NULL);
 
-	for(i = 0; s1[i]; i++)
+	for (i = 0; s1[i]; i++)
 		newstr[i] = s1[i];
 
 	if (cnt <= n)
