0bfe5a2b5cdb26642069cd4eadc9826d56fe5a02
R-INYURU <rwema.fred@tutamail.com>
2024-07-22 07:17:15 -0700

    initial commit

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define SIZE 1024
+
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
