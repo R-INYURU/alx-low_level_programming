59b2ec9e9b7900b1fdfaef6edd5be914e98d809b
R-INYURU <rwema.fred@tutamail.com>
2024-07-22 09:46:10 -0700

    yoo poppy

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -11,6 +11,9 @@ int main(void)
 	struct dog my_dog;
 
 	init_dog(&my_dog, "Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	printf("My name is %s, and I am %.1f :) - Woof!\n\n", my_dog.name, my_dog.age);
+	printf("Hey! this is %s, I own this s** dog called %s, and is %.1f old,\
+its sh*tty one, my I will kick out side if it doesn't get a job soon.\n",
+		   my_dog.owner, my_dog.name, my_dog.age);
 	return (0);
 }
