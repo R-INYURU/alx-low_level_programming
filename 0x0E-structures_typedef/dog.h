002c0901e51327b12c40a934a8aa0f5eee290535
R-INYURU <rwema.fred@tutamail.com>
2024-07-22 09:04:51 -0700

    betty baby

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - structure about the dog name, it's age, and owner
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner
+ */
 struct dog
 {
 	char *name;
