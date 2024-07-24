3626cfa45565c3a8fc777efede6b70105d90971d
R-INYURU <rwema.fred@tutamail.com>
2024-07-24 15:24:10 -0700

    move it inside

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
-#include <stddef.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
+#include <stddef.h>
 
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name_as_is(char *name);
