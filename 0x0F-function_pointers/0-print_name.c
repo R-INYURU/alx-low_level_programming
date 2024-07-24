e86cd660070314270848cb07e5046ae06cb1b070
R-INYURU <rwema.fred@tutamail.com>
2024-07-24 14:35:42 -0700

    what's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name from a fucntion passed as argument
+ * @name: name to print
+ * @f: function to print the name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
