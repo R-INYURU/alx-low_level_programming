52e5e514b01b7a8927d2e16cf0606e368edb9012
R-INYURU <rwema.fred@tutamail.com>
2024-07-25 15:08:43 -0700

    handle exceptions

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -8,5 +8,7 @@
 int main(void)
 {
 	print_numbers(", ", 4, 0, 98, -1024, 402);
+	print_numbers("", 3, 1, 3, 5);
+	print_numbers("-", 0, 1, 2, 3);
 	return (0);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * print_numbers - print the given numbers end with a newline
@@ -14,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list arg;
 	unsigned int i;
 
-	if (!separator || n == 0)
+	if (!strcmp(separator, "") || n == 0)
 		return;
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
