5eb1f1fb1ec10cf47e6276c817dff8ce6d0e1737
R-INYURU <rwema.fred@tutamail.com>
2024-08-04 06:14:18 -0700

    first linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to list
+ *
+ * Return: size of the linked list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count ++;
+	}
+	return (count);
+}
