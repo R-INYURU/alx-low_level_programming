ea1bee557ba81716d896e2af561a416d17e7e0f4
R-INYURU <rwema.fred@tutamail.com>
2024-08-04 13:25:43 -0700

    this first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+void __attribute__ ((constructor)) first(void);
+
 /**
  * first - prints given text before main is executed
  *
  * Return: void
  */
-void __attribute__ ((constructor)) first()
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house my back!\n");
 }
