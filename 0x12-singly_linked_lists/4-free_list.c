81395cb2174f185c0d8018315aa58b9f3e10d864
R-INYURU <rwema.fred@tutamail.com>
2024-08-04 11:18:32 -0700

    freeing node space

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees the memory from list
+ * @head: head node
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *rand;
+	while (head != NULL)
+	{
+		rand = head->next;
+		free(head->str);
+		free(head);
+		head = rand;
+	}
+}
