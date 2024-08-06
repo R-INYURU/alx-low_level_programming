eda8ae8af838b7f740c125c08f9e4b23302296ce
R-INYURU <rwema.fred@tutamail.com>
2024-08-06 03:53:07 -0700

    initial

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all elements in listint_t list
+ * @h: head node of the list
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		
+	}
+	return (count);
+}
