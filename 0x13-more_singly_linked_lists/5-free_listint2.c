580896458ad0fa591853b7345530ac90e3ef16de
R-INYURU <rwema.fred@tutamail.com>
2024-08-06 09:35:40 -0700

    free mn

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - func that frees listint_t
+ * @head: head of the list node
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while (*head != NULL)
+	{
+		node = (*head)->next;
+		free(*head);
+		*head = node;
+	}
+}
