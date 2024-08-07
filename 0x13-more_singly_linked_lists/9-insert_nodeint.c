8feb2668f5c85011b126ac446dba2403bfdb57fc
R-INYURU <rwema.fred@tutamail.com>
2024-08-07 11:30:13 -0700

    OR not AND

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (count = 0; count < (idx - 1); count++)
 	{
-		if (copyNode == NULL && copyNode->next == NULL)
+		if (copyNode == NULL || copyNode->next == NULL)
 			return (NULL);
 		copyNode = copyNode->next;
 	}
