a5fd0c7a89c1e2a4d3afe6cfcaef6c7702665209
R-INYURU <rwema.fred@tutamail.com>
2024-08-08 15:06:03 -0700

    binary 0;

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,48 @@
 #include "main.h"
+#include <math.h>
+
+/**
+ * _atoi - convert string to integer
+ * @str: string to convert
+ *
+ * Return: integer value
+ */
+int _atoi(const char *str)
+{
+	int val = 0;
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		val = val * 10 + str[i] - '0';
+
+	return (val);
+}
 
 /**
  * binay_to_uint - converts a binary number to an unsigned int
- * @b: char pointer storing binary number
+ * @b: string variable storing binary number
  *
  * Return: converted number, or 0
  * if there is a char in the arg. diff from 0 or 1 b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int cnt = 0, i;
+	unsigned int i, bit, rmndr = 0;
+	unsigned int integer = 0, base = 1;
 
-	while 
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+	}
+	bit = _atoi(b);
+
+	for (i = 0; bit != 0; i++)
+	{
+		rmndr = bit % 10;
+		bit /= 10;
+		integer += rmndr * base;
+		base = base * 2;
+	}
+	return (integer);
+}
