beb99d9a5de3cc15745f212bf803b21c992d2659
R-INYURU <rwema.fred@tutamail.com>
2024-08-09 14:28:52 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,5 +9,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	
+	int i, j, cnt = 0;
+
+	if (s == NULL || accept == NULL)
+		return (0);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] ==  accept[j])
+			{
+				if (j > cnt)
+					cnt = j;
+			}
+		}
+	}
+	return (cnt);
 }
