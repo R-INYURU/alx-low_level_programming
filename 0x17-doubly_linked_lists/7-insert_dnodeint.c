1110a89b93715351d756d54f08741af92508032f
R-INYURU <rwema.fred@tutamail.com>
2024-09-22 23:50:55 +0200

    betty b*

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	ins_node = malloc(sizeof(dlistint_t));
 	if (ins_node == NULL)
 		return (NULL);
-	
+
 	if (idx == 0)
 	{
 		ins_node = add_dnodeint(h, n);
